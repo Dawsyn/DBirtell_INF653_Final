8ed7b66e72141ebea7da944b3489b5e84106489c
Dawsyn <48812637+Dawsyn@users.noreply.github.com>
2024-04-29 10:33:05 -0500

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,8 +2,8 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
+        if (allowedOrigins.includes('*') || allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
